35f9c08ddd63f8e62c7fe4d07898f9acbe035a5f
Kevin-Lamanna <kevin.lamanna2@gmail.com>
2024-01-02 03:41:14 -0500

    Added Number and Back inputs

--- calculator.js
+++ calculator.js
@@ -11,13 +11,58 @@ function multiply(x, y) {
 };
 
 function divide(x, y) {
-    return x / y;
+    if (y == 0) {
+        throw "Cannot divide by zero";
+    }
+    try {
+        return x / y;
+    } catch (error) {
+        document.getElementById("my_error").innerHTML = error.message;
+    }
 };
 
-function clear() {
+function reciprocol(x) {
+    if (x == 0) {
+        throw "Cannot divide by zero";
+    }
+    try {
+        return 1 / x;
+    } catch (error) {
+        document.getElementById("my_error").innerHTML = error.message;
+    }
 
 }
 
-function back() {
+// Math.sqrt()
+// Math.pow(x, y)
 
+function clearCalc() {
+    document.getElementById("calculation").innerHTML = "";
+    document.getElementById("result").innerHTML = 0;
+    console.log("Cleared");
 }
+
+function backRemove() {
+    const newSlicedCalculation = document.getElementById("calculation").innerHTML.slice(0, -1);
+    document.getElementById("calculation").innerHTML = newSlicedCalculation;
+}
+
+function percent() {
+    document.getElementById("result").innerHTML /= 100;
+}
+
+function showNum(num) {
+    document.getElementById("calculation").innerHTML += num;
+    console.log(document.getElementById("calculation").innerHTML);
+}
+
+
+// const nums = document.querySelectorAll('.calNum');
+// nums.forEach(num => num.addEventListener('click', showNum(num.innerHTML)));
+
+// const CE = document.getElementById('ceBtn');
+// CE.addEventListener('onclick', clear());
+
+// const C = document.getElementById('clearBtn');
+// C.addEventListener('onclick', clear());
+
