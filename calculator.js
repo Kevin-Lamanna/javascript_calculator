2fb553e51df46cc167ae03989ad69546dd2bc632
Kevin-Lamanna <kevin.lamanna2@gmail.com>
2024-01-03 01:02:15 -0500

    Added Basic Functionality

--- calculator.js
+++ calculator.js
@@ -1,29 +1,13 @@
-function add(x, y) {
-    return x + y;
-};
-
-function subtract(x, y) {
-    return x - y;
-};
-
-function multiply(x, y) {
-    return x * y;
-};
-
 function divide(x, y) {
     if (y == 0) {
-        throw "Cannot divide by zero";
+        throw new Error("Cannot divide by zero");
     }
-    try {
-        return x / y;
-    } catch (error) {
-        document.getElementById("my_error").innerHTML = error.message;
-    }
+    return x / y;
 };
 
 function reciprocol(x) {
     if (x == 0) {
-        throw "Cannot divide by zero";
+        throw new Error("Cannot divide by zero");
     }
     try {
         return 1 / x;
@@ -43,8 +27,15 @@ function clearCalc() {
 }
 
 function backRemove() {
-    const newSlicedCalculation = document.getElementById("calculation").innerHTML.slice(0, -1);
-    document.getElementById("calculation").innerHTML = newSlicedCalculation;
+    if (document.getElementById("result").innerHTML != "Cannot divide by zero") {
+        const newSlicedCalculation = document.getElementById("calculation").innerHTML.slice(0, -1);
+        document.getElementById("calculation").innerHTML = newSlicedCalculation;
+    }
+    else {
+        document.getElementById("calculation").innerHTML = "";
+        document.getElementById("result").innerHTML = 0;
+    }
+
 }
 
 function percent() {
@@ -56,13 +47,62 @@ function showNum(num) {
     console.log(document.getElementById("calculation").innerHTML);
 }
 
+function showOperation(op) {
+    const op_arr = ['+', '-', 'x', '÷'];
+    const last_input = document.getElementById("calculation").innerHTML.slice(-1);
+    if (last_input == '=') {
+        document.getElementById("calculation").innerHTML = document.getElementById("result").innerHTML;
+    }
+    if (!op_arr.includes(last_input)) {
+        document.getElementById("calculation").innerHTML += op;
+    }
+    else {
+        document.getElementById("calculation").innerHTML = document.getElementById("calculation").innerHTML.slice(0, -1);
+        document.getElementById("calculation").innerHTML += op;
+    }
+}
 
-// const nums = document.querySelectorAll('.calNum');
-// nums.forEach(num => num.addEventListener('click', showNum(num.innerHTML)));
+function expressType() {
+    const op_arr = ['+', '-', 'x', '÷'];
+    const calculationStr = document.getElementById("calculation").innerHTML;
+    for (let i = 0; i < op_arr.length; i++) {
+        if (calculationStr.includes(op_arr[i])) {
+            return op_arr[i];
+        };
 
-// const CE = document.getElementById('ceBtn');
-// CE.addEventListener('onclick', clear());
+    }
 
-// const C = document.getElementById('clearBtn');
-// C.addEventListener('onclick', clear());
+}
 
+function evaluateCalc() {
+    const exType = expressType();
+    const calculationStr = document.getElementById("calculation").innerHTML;
+
+    const calcArray = calculationStr.split(exType);
+
+    let num1 = parseFloat(calcArray[0]);
+    let num2 = parseFloat(calcArray[1]);
+
+
+    if (exType == '+') {
+        document.getElementById("result").innerHTML = num1 + num2;
+    }
+    if (exType == '-') {
+        document.getElementById("result").innerHTML = num1 - num2;
+    }
+    if (exType == 'x') {
+        document.getElementById("result").innerHTML = num1 * num2;
+    }
+    if (exType == '÷') {
+        try {
+            document.getElementById("result").innerHTML = divide(num1, num2);
+        }
+        catch (error) {
+            document.getElementById("result").innerHTML = error.message;
+        }
+
+    }
+
+    //document.getElementById("calculation").innerHTML = document.getElementById("result").innerHTML;
+    document.getElementById("calculation").innerHTML += '=';
+}
